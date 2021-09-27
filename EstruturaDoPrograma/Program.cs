0fc8ff3b15559a132d316e03499e8bca8ac936cb
Henrique Marques <hfvmarques@gmail.com>
2021-09-27 17:18:51 -0400

    Estrutura do Programa

---
+++ EstruturaDoPrograma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EstruturaDoPrograma.Exemplos;
+
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            System.Console.WriteLine(s.Desempilha());
+            System.Console.WriteLine(s.Desempilha());
+            System.Console.WriteLine(s.Desempilha());
+            System.Console.WriteLine(s.Desempilha());
+        }
+    }
+}
